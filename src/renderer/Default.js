f6825f5a6dbe47632fd3a2dd5c008a62a0292f68
Juan Cortes Ross <juan@bohem.io>
2020-03-12 12:01:37 +0100

    wip Swap - Form screen, Landing, Device connect, Modal WIP

--- src/renderer/Default.js
+++ src/renderer/Default.js
@@ -36,6 +36,7 @@ import Page from "~/renderer/components/Page";
 import AnalyticsConsole from "~/renderer/components/AnalyticsConsole";
 import ModalsLayer from "./ModalsLayer";
 import DebugMock from "~/renderer/components/DebugMock";
+import Swap from "~/renderer/screens/swap";
 
 const reloadApp = event => {
   if ((event.ctrlKey || event.metaKey) && event.key === "r") {
@@ -103,6 +104,7 @@ const Default = () => {
                       />
                       <Route path="/account/:id" render={props => <Account {...props} />} />
                       <Route path="/asset/:assetId+" render={props => <Asset {...props} />} />
+                      <Route path="/swap" render={props => <Swap {...props} />} />
                     </Switch>
                   </Page>
                 </Box>
